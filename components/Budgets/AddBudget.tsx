2d8e276fd6992752aba5124a718783d4b0a505c5
victorrdias <vi_dias_15@hotmail.com>
2022-05-21 18:43:41 -0300

    create expense form and style

--- components/Budgets/AddBudget.tsx
+++ components/Budgets/AddBudget.tsx
@@ -15,63 +15,66 @@ const AddBudget = () => {
       budget: "",
     },
   });
-  const budgets = ["Alimentação", "Gasolina"];
+  const budgets = ["Alimentação", "Gasolina", "MACONHA"];
 
   const submitBudgetForm = (data) => console.log("data", data);
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit(submitBudgetForm)}>
-      <h3>Adicionar entrada</h3>
-      <div className={styles.formControl}>
-        <label htmlFor="description">Descrição</label>
-        <input
-          placeholder="Descrição sobre a entrada"
-          {...register("description", {
-            required: { value: true, message: "Digite uma descrição" },
-          })}
-        />
-        <span className={styles.errorMessage}>
-          {errors.description && errors.description.message}
-        </span>
-      </div>
-      <div className={styles.formControl}>
-        <label htmlFor="amount">Valor do Orçamento</label>
-        <input
-          placeholder="R$00,00"
-          type="number"
-          {...register("amount", {
-            required: { value: true, message: "Digite um valor" },
-          })}
-        />
-        <span className={styles.errorMessage}>
-          {errors.amount && errors.amount.message}
-        </span>
-      </div>
-      <div className={styles.formControl}>
-        <label htmlFor="budget">Categoria</label>
-        <select
-          id="budget"
-          {...register("budget", {
-            required: { value: true, message: "Escolha uma categoria" },
-          })}
-        >
-          <option value="" disabled>
-            Categoria do Orçamento
-          </option>
-          {budgets.map((budget) => (
-            <option value={budget} key={budget}>
-              {budget[0].toUpperCase() + budget.slice(1)}
+    <div className={styles.formContainer}>
+      <form className={styles.form} onSubmit={handleSubmit(submitBudgetForm)}>
+        <h3>Adicionar gasto</h3>
+        <div className={styles.formControl}>
+          <label htmlFor="description">Descrição</label>
+          <input
+            placeholder="Descrição sobre o gasto"
+            {...register("description", {
+              required: { value: true, message: "Digite uma descrição" },
+            })}
+          />
+          <span className={styles.errorMessage}>
+            {errors.description && errors.description.message}
+          </span>
+        </div>
+        <div className={styles.formControl}>
+          <label htmlFor="amount">Valor do gasto</label>
+          <input
+            placeholder="R$00,00"
+            type="number"
+            step={0.01}
+            {...register("amount", {
+              required: { value: true, message: "Digite um valor" },
+            })}
+          />
+          <span className={styles.errorMessage}>
+            {errors.amount && errors.amount.message}
+          </span>
+        </div>
+        <div className={styles.formControl}>
+          <label htmlFor="budget">Categoria</label>
+          <select
+            id="budget"
+            {...register("budget", {
+              required: { value: true, message: "Escolha uma categoria" },
+            })}
+          >
+            <option value="" disabled>
+              Categoria do Gasto
             </option>
-          ))}
-        </select>
-        <span className={styles.errorMessage}>
-          {errors.budget && errors.budget.message}
-        </span>
-      </div>
-      <button type="submit" className={styles.submitButton}>
-        Adicionar
-      </button>
-    </form>
+            {budgets.map((budget) => (
+              <option value={budget} key={budget}>
+                {budget[0].toUpperCase() + budget.slice(1)}
+              </option>
+            ))}
+          </select>
+          <span className={styles.errorMessage}>
+            {errors.budget && errors.budget.message}
+          </span>
+        </div>
+        <button type="submit" className={styles.submitButton}>
+          Adicionar
+        </button>
+      </form>
+    </div>
   );
 };
 
