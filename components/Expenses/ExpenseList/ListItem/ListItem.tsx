de1f85b42ec6d82e643e30f64f349db1939e2a3d
cptvictor95 <cpt.victor@hotmail.com>
2022-06-01 14:12:15 -0300

    fix date format on expense list item

--- components/Expenses/ExpenseList/ListItem/ListItem.tsx
+++ components/Expenses/ExpenseList/ListItem/ListItem.tsx
@@ -6,12 +6,10 @@ const ListItem: React.FC<{ expense: Expense }> = ({ expense }) => {
   const day = new Date(expense.createdAt).getUTCDate();
   const month = new Date(expense.createdAt).getUTCMonth() + 1;
 
-  const formattedDate = `${day} / ${month - 10 ? `0${month}` : month}`;
-  console.log({
-    day,
-    month,
-    formattedDate,
-  });
+  const formattedDate = `${day < 10 ? `0${day}` : day} / ${
+    month < 10 ? `0${month}` : month
+  }`;
+
   return (
     <div className={styles.listItem}>
       <p>{expense.description}</p>
