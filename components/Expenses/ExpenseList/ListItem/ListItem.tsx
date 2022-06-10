0730cae62a57143892eb695c9cc1026f2239eee0
victorrdias <vi_dias_15@hotmail.com>
2022-06-10 17:40:18 -0300

    fix category name and import from firebase

--- components/Expenses/ExpenseList/ListItem/ListItem.tsx
+++ components/Expenses/ExpenseList/ListItem/ListItem.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import { FirebaseCtx } from "@/config/context";
+import { BudgetCtx } from "@/contexts/BudgetContext";
+import Budgets from "pages/budgets";
+import React, { useContext, useState } from "react";
+import { Budget } from "types/Budget";
 import { Expense } from "types/Expense";
 import styles from "./styles.module.scss";
+
 /**
  * @todo
  * create getBugdetById
@@ -9,19 +14,46 @@ const ListItem: React.FC<{ expense: Expense; index: number }> = ({
   expense,
   index,
 }) => {
+  const [myBudgetName, setMyBudgetName] = useState<string | undefined>();
   const day = new Date(expense.createdAt).getUTCDate();
   const month = new Date(expense.createdAt).getUTCMonth() + 1;
 
   const formattedDate = `${day < 10 ? `0${day}` : day} / ${
     month < 10 ? `0${month}` : month
   }`;
+  const { firestore } = useContext(FirebaseCtx);
+  const { getUserBudgets } = useContext(BudgetCtx);
 
+  const getBudgetById = async (budgetId: string) => {
+    try {
+      const budgetRef = firestore.collection("budgets").doc(budgetId);
+
+      const budget = await budgetRef.get().then((response) => {
+        const data = response.data() as Budget;
+        if (!data) console.error("Erro ao encontrar categoria.");
+        return data;
+      });
+      return budget;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  const handleGetBudgetName = async (budgetId: string) => {
+    const myBudget = await getBudgetById(budgetId).then((response) => {
+      return response.name;
+    });
+    setMyBudgetName(myBudget);
+    return myBudget;
+  };
+  handleGetBudgetName(expense.budgetId);
   return (
     <li className={styles.listItem}>
       <p>{index + 1}</p>
       <p>{expense.description}</p>
       <p>R${expense.amount}</p>
-      <p>{expense.budgetId}</p>
+      <p>{myBudgetName}</p>
       <p>{formattedDate}</p>
       <div className={styles.options}>
         <button className={styles.editButton}>ed</button>
