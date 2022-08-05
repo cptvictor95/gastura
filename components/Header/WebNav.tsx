435bcb958f3dd8a42d6628d7dc1507dcf0e1a240
victorrdias <vi_dias_15@hotmail.com>
2022-08-04 21:13:52 -0300

    create option header for mobile

---
+++ components/Header/WebNav.tsx
@@ -0,0 +1,72 @@
+import useLoggedInUser from "@/hooks/useLoggedInUser";
+import { Button, Flex, Image, Spacer } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React from "react";
+
+const WebNav = () => {
+  const router = useRouter();
+  const { user, authState, handleUserLogout } = useLoggedInUser();
+
+  return (
+    <>
+      {authState === "LOADING" ? (
+        <></>
+      ) : user ? (
+        <Flex
+          display={{ base: "none", md: "flex", lg: "flex", xl: "flex" }}
+          minHeight="10vh"
+          as="header"
+          flexDirection="row"
+          align="center"
+          bgColor="green.900"
+          px="12"
+        >
+          <Image src="./assets/Logo.svg" boxSize="90px" alt="logoImage" />
+          <Spacer />
+          <Flex as="nav" gap="20">
+            <Button variant="headerBtn" onClick={() => router.push("/")}>
+              Home
+            </Button>
+
+            <Button
+              variant="headerBtn"
+              onClick={() => router.push("/expenses")}
+            >
+              Gastos
+            </Button>
+            <Button variant="headerBtn" onClick={() => router.push("/budgets")}>
+              Entradas
+            </Button>
+            <Button
+              bgColor="beige.100"
+              variant="headerBtn"
+              color="black"
+              onClick={handleUserLogout}
+            >
+              Sair
+            </Button>
+          </Flex>
+        </Flex>
+      ) : (
+        <Flex as="nav" gap="20">
+          <Button variant="headerBtn" onClick={() => router.push("/")}>
+            Home
+          </Button>
+          <Button variant="headerBtn" onClick={() => router.push("/login")}>
+            Entrar
+          </Button>
+          <Button
+            variant="headerBtn"
+            bgColor="beige.100"
+            color="black"
+            onClick={() => router.push("/register")}
+          >
+            Criar conta
+          </Button>
+        </Flex>
+      )}
+    </>
+  );
+};
+
+export default WebNav;
